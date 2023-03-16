5ca71d01e0bd81944b5d5a03aebff95701200d2c
wanguya <wanguyajay@gmail.com>
2023-03-16 15:39:48 +0300

    blogs

---
+++ fullstack/server/blogsBackend/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const articleRouter = require('./routes/articles')
+const app = express()
+
+app.set('view engine', "ejs")
+
+app.use("/ articles", articleRouter)
+
+app.get("/", (req, res) => {
+    const articles = [{
+        title: "test article",
+        createdAt: new Date(),
+        description: "test description"
+    },
+    {
+        title: "test article2",
+        createdAt: new Date(),
+        description: "test description2"
+    }]
+    res.render("articles/index", {articles: articles})
+})
+app.listen(5000)
