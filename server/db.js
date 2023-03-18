6ce88a7c2a1b323ec56c9773091703982fd5e8de
CodeDroid99 <johnmwega2002@gmail.com>
2023-03-18 18:50:05 +0300

    mongodb connection

---
+++ server/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+//connect to database
+mongoose.connect('mongodb+srv://ADMIN:mEvbNxBTN1ks6r8X@catcomdatabase.tnlvvk6.mongodb.net/test',{ useNewUrlParser: true, useUnifiedTopology: true },(err) => {
+    if (!err) { console.log('MongoDB connection succesful...'); }
+    else { console.log('MongoDB connection error.....: ' + JSON.stringify(err, undefined, 2)); }
+});
