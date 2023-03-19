a9fbc0506725ac3f9f71c9445b6ae7bde20247ba
CodeDroid99 <johnmwega2002@gmail.com>
2023-03-19 19:20:56 +0300

    delete

--- server/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-//connect to database
-mongoose.connect('mongodb+srv://ADMIN:mEvbNxBTN1ks6r8X@catcomdatabase.tnlvvk6.mongodb.net/test',{ useNewUrlParser: true, useUnifiedTopology: true },(err) => {
-    if (!err) { console.log('MongoDB connection succesful...'); }
-    else { console.log('MongoDB connection error.....: ' + JSON.stringify(err, undefined, 2)); }
-});
-module.exports=mongoose
