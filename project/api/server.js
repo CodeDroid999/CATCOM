b02acf0a2919b50b496f3ec16a00993028993bd5
CodeDroid99 <johnmwega2002@gmail.com>
2023-03-20 21:48:07 +0300

    use direct string

--- project/api/server.js
+++ project/api/server.js
@@ -1,7 +1,33 @@
 //create ex[ress server
 const express = require("express");
-
 const app = express();
+
+
+
+//configure db
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const MONGODB_URI = process.env;
+console.log(MONGODB_URI);
+
+//database connnection
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://ADMIN:mEvbNxBTN1ks6r8X@catcomdatabase.tnlvvk6.mongodb.net/test').then(console.log('MongoDB connection succesful...')).catch((err)=>console.log(err));
 
 // start server
 app.listen("5000", () => console.log(`Express server connection succesful....server started at port : 5000`));
+
+/*
+mongoose.connect('mongodb+srv://ADMIN:mEvbNxBTN1ks6r8X@catcomdatabase.tnlvvk6.mongodb.net/test', 
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(console.log('MongoDB connection succesful...'));
+// start server
+app.listen("5000", () => console.log(`Express server connection succesful....server started at port : 5000`));*/
+
+
+/*
+
+app.use("/", (req, res) => {
+    console.log("Express server connection succesful")});
+*/
