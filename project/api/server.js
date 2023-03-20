1eab5366500c9b54281028aec3a49377f3761c7d
CodeDroid99 <johnmwega2002@gmail.com>
2023-03-20 18:14:47 +0300

    // start server

--- project/api/server.js
+++ project/api/server.js
@@ -1,1 +1,7 @@
-const express = require(express);
+//create ex[ress server
+const express = require("express");
+
+const app = express();
+
+// start server
+app.listen("5000", () => console.log(`Express server connection succesful....server started at port : 5000`));
