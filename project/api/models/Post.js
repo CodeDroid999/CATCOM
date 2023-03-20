a13198d1ab7bd5c19ecae774362d0d3395e7f018
CodeDroid99 <johnmwega2002@gmail.com>
2023-03-21 00:01:53 +0300

    initiaize client side

--- project/api/models/Post.js
+++ project/api/models/Post.js
@@ -1,33 +1,30 @@
-const mongoose = rquire("mongoose");
+const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
+const PostSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     desc: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    featuredImage: {
-        type: String,
-        required: true,
-    },
-    profilePic: {
-        type: String,
-        required:true
+    photo: {
+      type: String,
+      required: false,
     },
     username: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    categories:{
-        type: Array,
-        required:true
-    }
-},
-    { timestamps: true }
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Post", PostSchema);
--- project/api/server.js
+++ project/api/server.js
@@ -1,33 +1,41 @@
-//create ex[ress server
 const express = require("express");
 const app = express();
-
-
-
-//configure db
 const dotenv = require("dotenv");
-dotenv.config();
-
-
-const MONGODB_URI = process.env;
-console.log(MONGODB_URI);
-
-//database connnection
 const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://ADMIN:mEvbNxBTN1ks6r8X@catcomdatabase.tnlvvk6.mongodb.net/test').then(console.log('MongoDB connection succesful...')).catch((err)=>console.log(err));
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
-// start server
-app.listen("5000", () => console.log(`Express server connection succesful....server started at port : 5000`));
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
-/*
-mongoose.connect('mongodb+srv://ADMIN:mEvbNxBTN1ks6r8X@catcomdatabase.tnlvvk6.mongodb.net/test', 
-{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(console.log('MongoDB connection succesful...'));
-// start server
-app.listen("5000", () => console.log(`Express server connection succesful....server started at port : 5000`));*/
+mongoose.connect("mongodb+srv://ADMIN:mEvbNxBTN1ks6r8X@catcomdatabase.tnlvvk6.mongodb.net/catcom",)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-/*
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
-app.use("/", (req, res) => {
-    console.log("Express server connection succesful")});
-*/
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
