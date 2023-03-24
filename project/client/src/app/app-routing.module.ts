abaecd625b32cf0cf6e2563b2bfbbb533b6cdc27
CodeDroid99 <johnmwega2002@gmail.com>
2023-03-24 16:20:48 +0300

    finetune

--- project/client/src/app/app-routing.module.ts
+++ project/client/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 const routes: Routes = [
- 
+ {path: "", component: HomeComponent},
+ {path:"**",component: NotFoundComponent},
+ {path:"contact-us", component: ContactsComponent}
 ];
 
 @NgModule({
--- project/client/src/app/app.module.ts
+++ project/client/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
