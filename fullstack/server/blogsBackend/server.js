4827f66a16aebf2d72c46e677b461775a2bfd355
wanguya <wanguyajay@gmail.com>
2023-03-25 01:25:26 +0300

    blogs

--- fullstack/server/blogsBackend/server.js
+++ fullstack/server/blogsBackend/server.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect("mongodb://localhost:27017/blogs",{
+    useNewUrlParser:true, useUnifiedTopology:true
+})
+
 app.set('view engine', "ejs")
 
-app.use("/ articles", articleRouter)
+
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
     const articles = [{
@@ -19,4 +25,5 @@ app.get("/", (req, res) => {
     }]
     res.render("articles/index", {articles: articles})
 })
+app.use("/articles", articleRouter)
 app.listen(5000)
