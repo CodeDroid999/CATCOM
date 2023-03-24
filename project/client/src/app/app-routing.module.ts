fd8abc3362cf8deef1d3e5332c2597d555f5344d
CodeDroid99 <johnmwega2002@gmail.com>
2023-03-24 18:32:12 +0300

    add routing

--- project/client/src/app/app-routing.module.ts
+++ project/client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { AlumniComponent } from './components/alumni/alumni.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { HomeComponent } from './components/home/home.component';
@@ -7,6 +8,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 const routes: Routes = [
+  {path: "about-us", component: AboutComponent},
   {path: "alumni", component: AlumniComponent},
   { path: 'contacts', component: ContactsComponent},
   { path: "", component: HomeComponent },
